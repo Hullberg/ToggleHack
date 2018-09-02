0dcfebf15a7e89f9d23a02ed3159d167ea2860cd
Hullberg <johan.hullberg@gmail.com>
2018-09-02 14:36:20 +0200

    changed to sha512 to bcrypt with salt through password_hash and verify

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -32,9 +32,10 @@ class UsersController {
 			else {
 				$pass1 = md5($password);
 				$salt = "This!sAStringT0AddSaltToTh3_Password";
-				$pass2 = hash("sha512", $salt . $password);
+                                $options = ['salt' => $salt];
+                                $pass2 = password_hash($password, PASSWORD_BCRYPT, $options);
                                 // Don't want SQL injections in register.
-				$insertion = $db->prepare('INSERT INTO users(username,md5pass,sha512pass) VALUES(:username, :pass1, :pass2)');
+				$insertion = $db->prepare('INSERT INTO users(username,md5pass,bcryptpass) VALUES(:username, :pass1, :pass2)');
                                 if($insertion->execute(array(':username' => $username, ':pass1' => $pass1, ':pass2' => $pass2))) {
                                     // If the execute returned true it worked
                                     $URL = "/ToggleHack/index.php";
@@ -56,33 +57,35 @@ class UsersController {
             $password = $_POST['loginpassword'];
             $pass1 = md5($password);
             $salt = "This!sAStringT0AddSaltToTh3_Password";
-            $pass2 = hash("sha512", $salt . $password);
+            $options = ['salt' => $salt];
             $db = Db::getInstance();
             $sqlsuccess = false;
             $sqllist = "This is what your query retrieved:\\n";
             
             if ($_SESSION['sql'] == 'ON') {
-                $login = $db->prepare("SELECT COUNT(*) as count FROM users WHERE username = :username AND md5pass = :pass1 AND sha512pass = :pass2");
-                $login->execute(array(":username" => htmlspecialchars($username), ":pass1" => $pass1, ":pass2" => $pass2));
+                $login = $db->prepare("SELECT * FROM users WHERE username = :username AND md5pass = :pass1");
+                $login->execute(array(":username" => htmlspecialchars($username), ":pass1" => $pass1));
                 $response = $login->fetch();
                 $uname = $response['username'];
+                $pwordcheck = password_verify($password, $response['bcryptpass']);
             } else {
                 //$test = "' or '1'='1' -- ";
-                $sql = "SELECT * FROM users WHERE username = '$username' AND md5pass = '$pass1' AND sha512pass = '$pass2'";
+                $sql = "SELECT * FROM users WHERE username = '$username' AND md5pass = '$pass1'";
                 $response = $db->query($sql)->fetchAll();
                 if (count($response) > 1) {
                     // SQL injection succeeded.
                     $sqlsuccess = true;
                     foreach ($response as $row) {
-                        $sqllist .= "Username: " . $row['username'] . ", md5pass: " . $row['md5pass'] . ", sha512pass: " . $row['sha512pass'] . "\\n";
+                        $sqllist .= "Username: " . $row['username'] . ", md5pass: " . $row['md5pass'] . ", bcryptpass: " . $row['bcryptpass'] . "\\n";
                     }
                 }
                 else {
                     $uname = $response[0]['username'];
+                    $pwordcheck = password_verify($password, $response[0]['bcryptpass']);
                 }
             }
             
-            if ($uname == $username) {
+            if ($uname == $username && $pwordcheck) {
                 // Username and passwords match
                 // We have previous output, so we use javascript to create cookie.
                 $URL = "/ToggleHack/index.php";
